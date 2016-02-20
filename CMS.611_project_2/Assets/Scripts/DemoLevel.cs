d61913aceda133350d00420766abb7cfd1c94e8f
jazzguitarist <richard@theoates.com>
2016-02-19 21:11:01 -0500

    initial pass at level classes

---
+++ CMS.611_project_2/Assets/Scripts/DemoLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DemoLevel : Level {
+
+	public override Dictionary<GameObject, int> getLevelChargeResources(){
+		GameObject positiveCharge = GameObject.Find("PositiveCharge");
+		GameObject negativeCharge = GameObject.Find ("NegativeCharge");
+		Dictionary<GameObject, int> resources = new Dictionary<GameObject, int> ();
+		resources.Add(positiveCharge, 3);
+		resources.Add (negativeCharge, 3);
+		return resources;
+	}
+}
---
+++ CMS.611_project_2/Assets/Scripts/Level.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class Level : MonoBehaviour {
+
+#region Fields
+	private Dictionary<GameObject, int> chargeResources;
+	private bool isComplete;
+#endregion
+
+#region Methods
+	private abstract Dictionary<GameObject, int> getLevelChargeResources();
+
+	public virtual bool levelCompleteCheck() {
+		return isComplete;
+	}
+#endregion
+
+#region Unity Callbacks
+	virtual void Start () {
+		chargeResources = new Dictionary<GameObject, int> ();
+		isComplete = false;
+	}
+#endregion
+}
