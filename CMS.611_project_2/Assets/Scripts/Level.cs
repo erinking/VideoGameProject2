10521dfccd09f0ed575880aba0411a7a25626189
David Ricardo <kigaltan@mit.edu>
2016-02-22 15:05:50 -0500

    Added a pusher that gives initial force and then deletes itself

--- CMS.611_project_2/Assets/Scripts/Level.cs
+++ CMS.611_project_2/Assets/Scripts/Level.cs
@@ -18,7 +18,7 @@ public abstract class Level : MonoBehaviour {
 	}
 
 	public bool tryConsumeCharge(GameObject prefab) {
-		print (chargeResources.Keys.Count);
+		//print (chargeResources.Keys.Count);
 		if (this.chargeResources.ContainsKey (prefab)) {
 			if (this.chargeResources [prefab] > 0) {
 				this.chargeResources[prefab] = this.chargeResources [prefab] - 1;
