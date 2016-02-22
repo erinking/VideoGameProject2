a7d2f8b599bc164e5065c861b13352337fb9d2a1
Erin King <erinking@mit.edu>
2016-02-22 15:04:34 -0500

    demo edits

--- CMS.611_project_2/Assets/Scripts/DemoLevel.cs
+++ CMS.611_project_2/Assets/Scripts/DemoLevel.cs
@@ -8,8 +8,8 @@ public class DemoLevel : Level {
 		GameObject positiveCharge = Resources.Load<GameObject> ("PositiveCharge");
 		GameObject negativeCharge = Resources.Load<GameObject> ("NegativeCharge");
 		Dictionary<GameObject, int> resources = new Dictionary<GameObject, int> ();
-		resources.Add (positiveCharge, 3);
-		resources.Add (negativeCharge, 3);
+		resources.Add (positiveCharge, 15);
+		resources.Add (negativeCharge, 15);
 		return resources;
 
 	}
