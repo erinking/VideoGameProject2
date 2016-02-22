a4e34bb56886214ba532dce3994125db71a55e79
David Ricardo <kigaltan@mit.edu>
2016-02-21 20:38:42 -0500

    There is now a goal! Check the console when it is hit.

---
+++ CMS.611_project_2/Assets/Scripts/GoalDetector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalDetector : MonoBehaviour {
+
+	bool isComplete;
+
+	void Start () {
+		this.isComplete = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("Goal was hit");
+	}
+
+}
