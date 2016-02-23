69d19ad858ddb14d669ab3f5b2febfc7b0312ab3
Erin King <erinking@mit.edu>
2016-02-23 17:39:18 -0500

    menu navigation works. need controls to return to menu, and need to fill in tutorial scene.

---
+++ CMS.611_project_2/Assets/Levels/Tutorial/TutorialLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TutorialLevel : Level {
+	
+	override public Dictionary<GameObject, int> getLevelChargeResources(){
+		GameObject positiveCharge = Resources.Load<GameObject> ("PositiveCharge");
+		GameObject negativeCharge = Resources.Load<GameObject> ("NegativeCharge");
+		Dictionary<GameObject, int> resources = new Dictionary<GameObject, int> ();
+		resources.Add (positiveCharge, 3);
+		resources.Add (negativeCharge, 3);
+		return resources;
+	}
+
+	override public void makeWallsAndGoalAndQuaffle() {
+
+	}
+}
---
+++ CMS.611_project_2/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+	// int level fron Build Settings
+	public void LoadScene(int level) {
+		Application.LoadLevel (level);
+	}
+}
