a7afcbb2a815000ab3258ffc67b86c33ac679290
David Ricardo <kigaltan@mit.edu>
2016-02-23 17:25:01 -0500

    Particles now have initial velocity

--- CMS.611_project_2/Assets/Scripts/SceneManager.cs
+++ CMS.611_project_2/Assets/Scripts/SceneManager.cs
@@ -8,6 +8,7 @@ public class SceneManager : MonoBehaviour {
 	public GameObject mouseInterface;
 	public Text posCount, negCount;
 	public CreateChargeOnClick mouseScript;
+	public Vector2 previousMousePosition;
 
 	// Use this for initialization
 	void Start () {
@@ -21,58 +22,74 @@ public class SceneManager : MonoBehaviour {
 			posCount.text = currentLevel.getNumCharges (mouseScript.posCharge);
 		if (negCount != null)
 			negCount.text = currentLevel.getNumCharges (mouseScript.negCharge);
+		this.previousMousePosition = Vector2.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			if (currentLevel.consumeCharge (mouseScript.posCharge)) {
-				Vector2 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-				Vector3 position = new Vector3 (mousePosition [0], mousePosition [1], 0);
-				GameObject newCharge = (GameObject) Instantiate (mouseScript.posCharge, position, Quaternion.identity);
-				CircleCollider2D[] chargeColliders = newCharge.GetComponents<CircleCollider2D> ();
-				CircleCollider2D innerCollider;
-				if (chargeColliders [0].radius < chargeColliders [1].radius) {
-					innerCollider = chargeColliders [0];
-				} else {
-					innerCollider = chargeColliders [1];
-				}
-				GameObject[] blockedRegions = GameObject.FindGameObjectsWithTag ("nope");
-				foreach (GameObject box in blockedRegions)  {
-					if (innerCollider.bounds.Intersects(box.GetComponent<BoxCollider2D>().bounds)) {
-						Debug.Log ("can't place a particle there!");
-						GameObject.Destroy (newCharge);
-						currentLevel.unconsumeCharge (mouseScript.posCharge);
-						break;
-					}
-				}
-				if (posCount != null)
-					posCount.text = currentLevel.getNumCharges (mouseScript.posCharge);
-			}
+		Vector2 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Vector2 mouseVelocity = (mousePosition - previousMousePosition) / Time.deltaTime;
+		if (Input.GetMouseButtonUp (0)) {
+			createCharge (1, mousePosition, mouseVelocity);
 		}
-		if (Input.GetMouseButtonDown (1)) {
-			if (currentLevel.consumeCharge (mouseScript.negCharge)) {
-				Vector2 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-				Vector3 position = new Vector3 (mousePosition [0], mousePosition [1], 0);
-				GameObject newCharge = (GameObject) Instantiate (mouseScript.negCharge, position, Quaternion.identity);
-				CircleCollider2D[] chargeColliders = newCharge.GetComponents<CircleCollider2D> ();
-				CircleCollider2D innerCollider;
-				if (chargeColliders [0].radius < chargeColliders [1].radius) {
-					innerCollider = chargeColliders [0];
-				} else {
-					innerCollider = chargeColliders [1];
-				}
-				GameObject[] blockedRegions = GameObject.FindGameObjectsWithTag ("nope");
-				foreach (GameObject box in blockedRegions)  {
-					if (innerCollider.bounds.Intersects(box.GetComponent<BoxCollider2D>().bounds)) {
-						GameObject.Destroy (newCharge);
-						currentLevel.unconsumeCharge (mouseScript.negCharge);
-						break;
-					}
-				}
-				if (negCount != null)
-					negCount.text = currentLevel.getNumCharges (mouseScript.negCharge);
-			}
+		if (Input.GetMouseButtonUp (1)) {
+			createCharge (-1, mousePosition, mouseVelocity);
 		}
+		this.previousMousePosition = mousePosition;
 	}
+
+	private void createCharge(int chargeValue, Vector2 position, Vector2 velocity){
+		/* Attempt to create a charge at the given position with the given velocity
+		 * @param chargeValue: the type of charge to create. Equals -1 for negative charges or +1 for other charges.
+		 * Does nothing if chargeValue is not -1 or +1.
+		 * @param position: where to attempt to create the charge. If position is illegal (e.g, it is in a place where
+		 * charges cannot go), this function will refuse to create it.
+		 * @param velocity: the charge's initial velocity.
+		 */
+
+		GameObject chargeToCreate = null;
+		if (chargeValue == -1) {
+			chargeToCreate = mouseScript.negCharge;
+			Debug.Log ("creating negative charge");
+		} else if (chargeValue == 1) {
+			chargeToCreate = mouseScript.posCharge;
+			Debug.Log ("creating positive charge");
+		} else {
+			Debug.Log ("Tried to create a charge that was not positive or negative.");
+		}
+			
+		if (currentLevel.consumeCharge (chargeToCreate)) {
+			Vector3 position3 = new Vector3 (position [0], position [1], 0);
+			GameObject newCharge = (GameObject) Instantiate (chargeToCreate, position, Quaternion.identity);
+			CircleCollider2D[] chargeColliders = newCharge.GetComponents<CircleCollider2D> ();
+			CircleCollider2D innerCollider;
+			if (chargeColliders [0].radius < chargeColliders [1].radius) {
+				innerCollider = chargeColliders [0];
+			} else {
+				innerCollider = chargeColliders [1];
+			}
+			GameObject[] blockedRegions = GameObject.FindGameObjectsWithTag ("nope");
+			foreach (GameObject box in blockedRegions)  {
+				if (innerCollider.bounds.Intersects(box.GetComponent<BoxCollider2D>().bounds)) {
+					Debug.Log ("can't place a particle there!");
+					GameObject.Destroy (newCharge);
+					currentLevel.unconsumeCharge (chargeToCreate);
+					break;
+				}
+			}
+
+			newCharge.GetComponent<Rigidbody2D> ().velocity = velocity;
+
+			if (chargeValue == 1 && posCount != null) {
+				posCount.text = currentLevel.getNumCharges (mouseScript.posCharge);
+			} else if (chargeValue == -1 && negCount != null) {
+				negCount.text = currentLevel.getNumCharges (mouseScript.negCharge);
+			}
+				
+		}
+
+
+
+	}
+
 }
